de4a8e100fb31c8c9af60f3a6865e9eec3e06f8a
Arnold <rnldlinus@gmail.com>
2023-07-14 19:35:15 +0300

    Created a function that gets the total distance covered for every instance of the trip

--- tsp.py
+++ tsp.py
@@ -44,34 +44,47 @@ def create_towns_dataframe(towns=None):
 
 def tsp(towns_df):
     # Create a list of cities
-    towns_df = towns_df["Town"].to_list()
+    towns_list = towns_df["Town"].to_list()
     # Shuffle the list of towns
+    shuffle(towns_df)
     # Create a distance list to append to
+    distance_list = []
     # Get the distance between the towns and append to the list
-    # return the total distance covered and the list of cities visited 
+    for i in range(len(towns_list)):
+        if i != len(towns_list) - 1:  # If i is not the last item in the list
+            start_point = towns_df[towns_df["Town"]
+                == towns_list[i]]["Lat"].values[0]
+            end_point = towns_df[towns_df["Town"]
+                == towns_list[i+1]]["Lat"].values[0]
+
+            dist = distance.distance((start_point, end_point).km
+            distance_list.append(dist)
+    # return the total distance covered and the list of cities visited
+    total_distance=sum(distance_list)
+    return total_distance, towns_list
 
 
 # Creating a click group
 
 
-@click.group()
+@ click.group()
 def cli():
     """This is a commandline tool that figurs out the shortest distance to visit all towns in a list"""
 
 # This command takes a varaible number of arguments of cities passed in
 
 
-@cli.command("towns")
-@click.argument("towns", nargs=-1)  # Takes any number of arguments
-@click.option("--count", default=5, help="The number of simulations to run")
+@ cli.command("towns")
+@ click.argument("towns", nargs=-1)  # Takes any number of arguments
+@ click.option("--count", default=5, help="The number of simulations to run")
 def cities_cli(cities, count):
     """This commmand-line tool figurs out the shortest distance to visit all the towns in a list
 
     Example: ./tsp.py towns "Nairobi" "Eldoret" --count 3
     """
     # Create a list of towns
-    towns_list = my_towns(*towns)
+    towns_list=my_towns(*towns)
     # Create a dataframe of cities and their lattitudes and longitudes
-    towns_df = create_towns_dataframe(towns_list)
+    towns_df=create_towns_dataframe(towns_list)
     # Running the TSP function
     main(count, towns_df)
